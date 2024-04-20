9c4f39aa5d63671d508603b3cbdfe6582418dd27
mevadapawan <130583824+mevadapawan@users.noreply.github.com>
2024-04-20 11:18:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font awsome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Swiperjs CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="css/index.css">
+    <!-- <link rel="stylesheet" href="./css/project.css"> -->
+    <!-- <link rel="stylesheet" href="./bootstrap/css/bootstrap-grid.min.css"> -->
+    <script src="./js/jquery-3.7.1.min.js"></script>
+
+    <!-- <script sr></script> -->
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    require "./header.php";
+    ?>
+<div class=" container1" id="home">
+    <div class="wrapper">
+        <div class="content">
+            <h2 style="font-weight:lighter;color:white;" >Hello!</h2>
+        <h1>I'm Mevada Pawan</h1>
+        <h2 class="h2">UI/UX Designer</h2>
+        <p> I'm a UX/UI Designer with a background in Psychology,<br>
+            which gives me a unique edge in empathixing<br>
+            with users and improving their experience<br>
+            through design    
+        </p>
+    </div>
+    <div class="image">
+        <img src="img/img-3.png" alt="">
+    </div>
+    </div>
+</div>
+
+<!-- Container 2 -->
+    <div class="container2 " id='about'>
+    <div class="row wrapper">
+        <div class="col-lg-4 imgBx">
+            <div class="image">
+                <img src="img/img-1.png" alt="">
+            </div>
+        </div>
+        <div class="col-lg-8">
+        <div class="aboutus">
+            <h1 class="text-center">About Us  </h1>
+            <h2>Hy, I'm Mevada Pawan |<span class='h2'> UI/UX Designer</span></h2>
+            <h3>Passionate about blending aesthetics with functionality,
+                I specialize in crafting seamless digital experiences as a 
+                UI/UX designer. With a Keen eye for detail and a user-centric 
+                approach, I transform ideas into intuitive designs that designs that
+                captivate and delight users.
+            </h3>
+            <div class="social-icon">
+                              <!-- Add icon here -->
+                <!-- <span class="icon"><i><a href=""><img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/facebook--v1.png" alt="facebook--v1"/></a></i></span> -->
+                                <!-- Add icon here -->
+                <span class="icon"><i><a href="https://www.instagram.com/uiux.pawan?igsh=eHZycHBtaXdrZGwz"><img width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1"/></a></i></span>
+                                <!-- Add icon here -->
+                <span class="icon"><i><a href="https://www.linkedin.com/in/mevada-pawan-19081b302?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img width="40" height="40" src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png" alt="linkedin"/></a></i></span>
+            </div>
+        </div>
+        </div>
+    </div>
+<!-- container-3 -->
+
+<div class="container3 " id='skills'>
+          <center><h1 >Skills</h1></center>
+          <div class="wrapper">
+
+            <div class="card">
+              <div class="card-body">
+                <!-- Add icon here -->
+
+              <h1 class="icon"><i><img width="90" height="90" src="https://img.icons8.com/carbon-copy/100/FFFFFF/media-queries.png" alt="media-queries"/></i></i></h1>
+              <h2>UI/UX Designer</h2>
+              <p class="card-text">Creating seamless journeys through pixels,where user delight meets design finesse</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+                              <!-- Add icon here -->
+
+              <h1 class="icon"><i><img width="90" height="90" src="https://img.icons8.com/external-others-zufarizal-robiyanto/64/FFFFFF/external-bulb-garis-online-learning-others-zufarizal-robiyanto.png" alt="external-bulb-garis-online-learning-others-zufarizal-robiyanto"/></i></i></h1>
+              <h2>Awesome Idea</h2>
+              <p class="card-text">As a UI/UX designer, I transform awesome ideas into captivating digital experiences, one pixel at a time</p>
+            </div>
+          </div>
+            <div class="card">
+                <div class="card-body">
+                    <!-- Add icon here -->
+
+                    <h1 class="icon"><i><img width="70" height="70" src="https://img.icons8.com/external-sbts2018-mixed-sbts2018/58/FFFFFF/external-wireframe-design-thinking2-sbts2018-mixed-sbts2018.png" alt="external-wireframe-design-thinking2-sbts2018-mixed-sbts2018"/></i></h1>
+                    <h2 class=" mb-2">Product Design</h2>
+                    <p class="card-text">I shape visionary product ideas into immersive digital realities, blending creativity with functionality</p>
+                  </div>
+            </div>
+                <div class="card">
+                    <div class="card-body">
+                          <!-- Add icon here -->
+
+                        <h1 class="icon"><i><img width="80" height="80" src="https://img.icons8.com/wired/64/FFFFFF/guarantee--v1.png" alt="guarantee--v1"/></i></i></h1>
+                        <h2 class="mb-2 ">High Quality</h2>
+                        <p class="card-text"> Designing excellence into every pixel, I am the UI/UX designer committed to delivering high-quality experiences that inspire and delight</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                    <!-- Add icon here -->
+
+                    <h1 class="icon"><i><img width="80" height="80" src="https://img.icons8.com/ios/50/FFFFFF/snap-background-to-white.png" alt="snap-background-to-white"/></i></i></h1>
+                        <h2 class=" mb-2">Pixel Perfect</h2>
+                        <p class="card-text">I meticulously craft perfect pixel designs,where every detail is a testament to precision and passion.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+            <!-- Add icon here -->
+
+                        <h1 class="icon"><i><img width="80" height="80" src="https://img.icons8.com/external-wanicon-lineal-wanicon/64/FFFFFF/external-brand-business-model-canvas-wanicon-lineal-wanicon.png" alt="external-brand-business-model-canvas-wanicon-lineal-wanicon"/></i></i></h1>
+                        <h2 class=" mb-2">Branding</h2>
+                        <p class="card-text">I blend creativity and strategy to shape branding designs that resonate deeply with audiences, leaving lasting impressions at every touchpoint.</p>
+                      </div>
+                </div>
+              </div>
+</div>
+<?php 
+    require './project.php';
+?>  
+
+ <?php
+     require './contactForm.php'; 
+   ?>
+
+
+
+
+
+<!-- Bootstrap CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="./js/index.js"></script>
+</body>
+</html>
